f45c635699b5b9051ae67d0234e47ac9bd9f97e2
hayao <shun819.mail@gmail.com>
2025-04-23 05:32:35 +0900

    feat: Add command flag support to root command

--- go/cmd/root.go
+++ go/cmd/root.go
@@ -1,15 +1,21 @@
 package cmd
 
 import (
+	"github.com/Hayao0819/nahi/cobrautils"
 	"github.com/Hayao0819/zash/go/shell"
 	"github.com/spf13/cobra"
 )
 
 func rootCmd() *cobra.Command {
+	var cmdStr string
 	cmd := &cobra.Command{
 		Use:   "zash",
 		Short: "Zash is a shell written in Go.",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if cmdStr != "" {
+				return cobrautils.CallCmd(cmd, *runCmd(), cmdStr)
+			}
+
 			s, err := shell.New()
 			if err != nil {
 				return err
@@ -20,6 +26,8 @@ func rootCmd() *cobra.Command {
 		SilenceErrors: true,
 		SilenceUsage:  true,
 	}
+	// cmd.Flags().StringP("command", "c", "", "command to execute")
+	cmd.Flags().StringVarP(&cmdStr, "command", "c", "", "command to execute")
 
 	cmd.AddCommand(runCmd())
 
