3c4b70ca2a6a7649aab23f46afda6837ccba60da
hayao <shun819.mail@gmail.com>
2025-04-22 16:03:23 +0900

    refactor: Hide readliine from shell

--- go/prompt/base.go
+++ go/prompt/base.go
@@ -1,16 +1,46 @@
 package prompt
 
+import (
+	"github.com/chzyer/readline"
+	"github.com/mattn/go-tty"
+)
+
 type Prompt struct {
 	user       string
 	currentDir string
 	exitCode   int
+	tty        *tty.TTY
 }
 
-func New() (*Prompt, error) {
-	p := Prompt{}
+func New(t *tty.TTY) (*Prompt, error) {
+	p := Prompt{
+		tty: t,
+	}
 	if err := p.Update(); err != nil {
 		return nil, err
 	}
 
 	return &p, nil
+}
+
+func (p *Prompt) WaitInput() (string, error) {
+	rl, err := readline.NewEx(&readline.Config{
+		Prompt: p.String(),
+		Stdin:  p.tty.Input(),
+		Stdout: p.tty.Output(),
+		Stderr: p.tty.Output(),
+	})
+	if err != nil {
+		return "", err
+	}
+	defer rl.Close()
+
+	line, err := rl.Readline()
+	if err != nil {
+		return "", err
+	}
+	if line == "" {
+		return "", nil
+	}
+	return line, nil
 }
--- go/shell/prompt.go
+++ go/shell/prompt.go
@@ -4,29 +4,20 @@ import (
 	"context"
 
 	"github.com/Hayao0819/zash/go/prompt"
-	"github.com/chzyer/readline"
 )
 
 func (s *Shell) WaitInputWithPrompt(ctx context.Context) string {
-
-	p, err := prompt.New()
+	p, err := prompt.New(s.TTY)
 	if err != nil {
 		s.Println(err.Error())
 		return ""
 	}
 
-	rl, err := readline.NewEx(&readline.Config{
-		Prompt: p.String(),
-		Stdin:  s.TTY.Input(),
-		Stdout: s.TTY.Output(),
-		Stderr: s.TTY.Output(),
-	})
+	r, err := p.WaitInput()
 	if err != nil {
 		s.Println(err.Error())
 		return ""
 	}
-	defer rl.Close()
 
-	r, _ := rl.Readline()
 	return r
 }
