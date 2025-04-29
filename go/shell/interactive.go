0f98d49de9aa278c4d1116c3b6378e8d87040ac5
hayao <shun819.mail@gmail.com>
2025-04-29 21:07:54 +0900

    fix: Improve error handling in StartInteractive by logging errors instead of printing

--- go/shell/interactive.go
+++ go/shell/interactive.go
@@ -1,5 +1,7 @@
 package shell
 
+import "github.com/Hayao0819/zash/go/internal/logmgr"
+
 func (s *Shell) StartInteractive() {
 	if s.started {
 		return
@@ -10,12 +12,15 @@ func (s *Shell) StartInteractive() {
 	for {
 		i, err := s.prompt.WaitInput()
 		if err != nil {
-			s.Println(err.Error())
+			// s.Println(err.Error())
+			logmgr.Shell().Error("ShellWaitInput", "error", err)
 			continue
 		}
 		if err := s.Run(i); err != nil {
 			s.Println(err.Error())
 		}
-		s.prompt.Update()
+		if err := s.prompt.Update(); err != nil {
+			logmgr.Shell().Error("ShellPromptUpdate", "error", err)
+		}
 	}
 }
